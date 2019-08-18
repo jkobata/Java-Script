12d8435efb0837b649b96280b9244ff8204ace9f
joaovitorkobata200@hotmail.com <joaovitorkobata200@hotmail.com>
2019-08-17 22:25:02 -0300

    chat

---
+++ server.js
@@ -0,0 +1,27 @@
+const app = require('express')();
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
+
+
+app.get('/chat', (req, res)=>{
+    res.sendFile(__dirname+'/index.html')
+});
+app.get('/nome', (req, res) =>{
+    res.sendFile(__dirname+'/nome.html')
+
+})
+http.listen(3000,() =>
+{
+console.log('ouvindo a porta 3000')
+});
+
+io.on('connect', (socket)=>{
+    console.log('new connect',socket.id);
+    socket.on('msg', (msg)=>{
+        console.log(msg)
+
+        socket.broadcast.emit('msg', msg);
+
+    })
+
+})
